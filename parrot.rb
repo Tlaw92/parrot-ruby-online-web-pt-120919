a41b6bb2b86266627e0e5c067e351828bf5e4e91
travis Lawson <travislawclimb@gmail.com>
2019-12-13 19:51:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,23 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+#what are arguments
+
+#methods are boxes that encapsulate some type of logic
+
+
+def example
+  name = "Travis"
+end
+
+def example_statement
+  puts name
+end
+
+example
+example_statement
+
+
+
+parrot.rb:15:in `example_statement': undefined local variable or method `name' for main:Object (NameError)
